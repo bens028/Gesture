8d0d200a38d868669ab6aaae5f11be5a27886f03
CIFamisaran <chase.famisaran@gmail.com>
2024-10-18 10:34:13 +0800

    testing source control #2

--- Python/cv.py
+++ Python/cv.py
@@ -3,7 +3,7 @@ from tkinter import Frame
 import cv2
 from PIL import Image, ImageTk
 #Hello this is testing for the git source control
-
+# mas pogi si alobin
 
 # Initialize Tkinter window
 window = tk.Tk()
