377bad7a8632c0ec28a78a8ff696548830790829
crux091 <rtcruz3@student.apc.edu.ph>
2024-10-18 11:15:13 +0800

    add comment

--- Python/gesture.py
+++ Python/gesture.py
@@ -12,6 +12,7 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=6, min_detection_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
 
+#hii
 
 
 def is_hi_sign(hand_landmarks):
