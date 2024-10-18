7cd2099ab62fd51b98df4acdd4fd4bce3e917e52
bens028 <vpalobin@student.apc.edu.ph>
2024-10-18 19:34:02 +0800

    ge

--- Python/gesture.py
+++ Python/gesture.py
@@ -13,7 +13,7 @@ hands = mp_hands.Hands(max_num_hands=6, min_detection_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
 
 #hii
-
+#aha7hdaybfysr
 
 def is_hi_sign(hand_landmarks):
     thumb_tip = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
